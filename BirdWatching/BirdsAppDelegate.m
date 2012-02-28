d3f94284b0d586915f7fc511d8bf07171c53c330
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-02-28 00:31:20 +0000

    Designing the Master Scene

--- BirdWatching/BirdsAppDelegate.m
+++ BirdWatching/BirdsAppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "BirdsAppDelegate.h"
 
+#import "BirdSightingDataController.h"
+#import "BirdsMasterViewController.h"
+
 @implementation BirdsAppDelegate
 
 @synthesize window = _window;
@@ -15,7 +18,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    //return YES;
+    
+    UINavigationController *navigationController = (UINavigationController*) self.window.rootViewController;
+    BirdsMasterViewController *firstViewController = (BirdsMasterViewController *) [[navigationController viewControllers] objectAtIndex:0];
+    
+    BirdSightingDataController *aDataController = [[BirdSightingDataController alloc] init];
+    firstViewController.dataController = aDataController;
     return YES;
+    
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
--- BirdWatching/BirdsMasterViewController.m
+++ BirdWatching/BirdsMasterViewController.m
@@ -10,12 +10,19 @@
 
 #import "BirdsDetailViewController.h"
 
+#import "BirdSightingDataController.h"
+#import "BirdSighting.h"
+
+/*
 @interface BirdsMasterViewController () {
     NSMutableArray *_objects;
 }
 @end
+*/
 
 @implementation BirdsMasterViewController
+
+@synthesize dataController = _dataController;
 
 
 - (void)awakeFromNib
@@ -27,10 +34,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    /*
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
+     */
 }
 
 - (void)viewDidUnload
@@ -44,6 +53,7 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+/*
 - (void)insertNewObject:(id)sender
 {
     if (!_objects) {
@@ -53,6 +63,7 @@
     NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
     [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
+*/
 
 #pragma mark - Table View
 
@@ -63,24 +74,44 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _objects.count;
+    //return _objects.count;
+    return [self.dataController countofList];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    /*UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
 
     NSDate *object = [_objects objectAtIndex:indexPath.row];
     cell.textLabel.text = [object description];
+    return cell;*/
+    
+    static NSString *CellIdentifier = @"BirdSightingCell";
+    static NSDateFormatter* formatter = nil;
+    
+    if(formatter == nil) {
+        formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateStyle:NSDateFormatterMediumStyle];
+    }
+    
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    BirdSighting *sightingAtIndex = [self.dataController objectInListAtIndex:indexPath.row];
+    [[cell textLabel] setText:sightingAtIndex.name];
+    [[cell detailTextLabel] setText:[formatter stringFromDate:(NSDate*)sightingAtIndex.date]];
     return cell;
+    
+    
+    
 }
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO; //YES;
 }
 
+/*
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -90,6 +121,7 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }
 }
+ */
 
 /*
 // Override to support rearranging the table view.
