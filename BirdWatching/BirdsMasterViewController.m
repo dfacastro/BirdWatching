a4e4619c841f8dc35cf3a80fe419f7631ca668c1
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-04-02 21:24:59 +0100

    Displaying Information in the Detail Scene
    3 Send data to the detail scene

--- BirdWatching/BirdsMasterViewController.m
+++ BirdWatching/BirdsMasterViewController.m
@@ -139,13 +139,25 @@
 }
 */
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+/*- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
         NSDate *object = [_objects	 objectAtIndex:indexPath.row];
         [[segue destinationViewController] setDetailItem:object];
     }
+}*/
+
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([[segue identifier] isEqualToString:@"ShowSightingDetails"]) {
+        BirdsDetailViewController *detailViewController = [segue destinationViewController];
+        
+        detailViewController.sighting = [self.dataController objectInListAtIndex:[self.tableView indexPathForSelectedRow].row];
+        
+    }
 }
+
+
 
 @end
