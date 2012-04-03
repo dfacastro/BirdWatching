6a3063a5970634c1587c330ddde56bbe0eab1c79
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-04-03 11:11:53 +0100

    Enabling the addition of new items
    1 Create the files for a new scene

---
+++ BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by Diogo Castro on 03/04/12.
+//  Copyright (c) 2012 FEUP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
